6cbb5d64ff0aef059e99081a71d8f45090c5c09b
camvar01 <milo.avc@gmail.com>
2025-10-25 16:08:44 -0500

    Recepcion de datos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FormsModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/componentes/agregar-empleado/agregar-empleado.ts
+++ src/app/componentes/agregar-empleado/agregar-empleado.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-agregar-empleado',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './agregar-empleado.html',
   styleUrl: './agregar-empleado.css',
 })
-export class AgregarEmpleado {
+export class AgregarEmpleado implements OnInit {
 
+  formularioDeEmpleado: FormGroup; 
+
+  constructor(public formulario: FormBuilder) {
+
+    this.formularioDeEmpleado = this.formulario.group({
+      nombre: [''],
+      correo: ['']
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  enviarDatos(): any{
+    console.log("Datos enviados");
+    console.log(this.formularioDeEmpleado.value);
+  }
 }
