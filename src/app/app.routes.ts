36f491b3eb7d020f64eaf80c2d3b305463041ce9
camvar01 <milo.avc@gmail.com>
2025-10-25 15:27:27 -0500

    Estructura y rutas basicas CRUD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AgregarEmpleado } from './componentes/agregar-empleado/agregar-empleado';
+import { EditarEmpleado } from './componentes/editar-empleado/editar-empleado';
+import { ListarEmpleado } from './componentes/listar-empleado/listar-empleado';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', pathMatch: 'full', redirectTo: 'agregar-empleado'},
+    {path: 'agregar-empleado', component: AgregarEmpleado},
+    {path: 'editar-empleado/:id', component: EditarEmpleado},
+    {path: 'listar-empleado', component: ListarEmpleado}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/componentes/listar-empleado/listar-empleado.ts
+++ src/app/componentes/listar-empleado/listar-empleado.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listar-empleado',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './listar-empleado.html',
   styleUrl: './listar-empleado.css',
 })
